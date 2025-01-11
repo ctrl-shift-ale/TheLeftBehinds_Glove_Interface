cffc909c17e277a1d761f48815a45177c92c99df
Alessandro Quaranta <alessandro_quaranta@hotmail.com>
2025-01-11 17:26:32 +0000

    add battery-test OSC message handler

--- Python/GloveInterface_osc_to_tdc.py
+++ Python/GloveInterface_osc_to_tdc.py
@@ -4,7 +4,7 @@ from pythonosc import osc_message_builder
 from pythonosc import udp_client
 import socket
 
-DEBUGGING = "NO_SENSORS" #"OFF", "SENSORS", "NO_SENSORS", "ALL"
+DEBUGGING = "BATTERY_TEST" #"OFF", "SENSORS", "NO_SENSORS", "ALL"
 
 esp32_ip = "192.168.1.100" # Static IP of the ESP32 device
 esp32_receive_port = 8000  # Port that the ESP32 listens for handshake request
@@ -44,9 +44,14 @@ def handle_battery(address, *args):
     message = f"/battery {' '.join(map(str, args))}"
     send_to_max(message)
 
+def handle_battery_test(address, *args):
+    if DEBUGGING == "BATTERY_TEST":
+        print(f"Battery Test: {args}")
+    
+
 # Function to handle incoming /sensors messages
 def handle_sensors(address, *args):
-    if DEBUGGING != "OFF" and DEBUGGING != "NO_SENSORS" :
+    if DEBUGGING != "OFF" and DEBUGGING != "NO_SENSORS" and DEBUGGING != "BATTERY_TEST" :
         print(f"Received /sensors message: {args}")
     message = f"/sensors {' '.join(map(str, args))}"
     send_to_max(message)
@@ -71,6 +76,7 @@ def start_osc_server():
     osc_dispatcher.map("/reply-handshake", handle_handshake_response)
     osc_dispatcher.map("/polling", handle_polling)
     osc_dispatcher.map("/battery", handle_battery)
+    osc_dispatcher.map("/battery-test", handle_battery_test)
     osc_dispatcher.map("/sensors", handle_sensors) 
     osc_dispatcher.map("/power-mode", handle_power_mode)
 
