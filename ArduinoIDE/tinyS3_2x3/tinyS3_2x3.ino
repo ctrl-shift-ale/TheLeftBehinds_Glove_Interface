1874c8d5ee51979bcfeeb9a461dc574f65c89a90
Alessandro Quaranta <alessandro_quaranta@hotmail.com>
2025-01-29 12:23:50 +0000

    remove multiple network credentials, keep LeftBehinds only

--- ArduinoIDE/tinyS3_2x3/tinyS3_2x3.ino
+++ ArduinoIDE/tinyS3_2x3/tinyS3_2x3.ino
@@ -1,4 +1,4 @@
-// this script is meant to work together with "Glove_Tiny_4x4.m4l" Max for Live device
+// this script is meant to work together with Glove_Tiny_2x3.amxd Max for Live device
 // please check https://github.com/ctrl-shift-ale/TheLeftBehinds_Glove_Interface
 
 #include <UMS3.h>
@@ -11,32 +11,28 @@
 const int N_INS = 5;
 const bool WIFI_MODE = true; // if false, then use serial to send osc messages
 const bool OFFLINE_MODE = false; // false -> try to connect to a wi-Fi endlessly at init. true -> switch to serial after reaching the connection timeout Limit
-const bool HYBERNATE_MODE = true; // goes in power save mode if wi-Fi connection timeout, with periodic polling (HYBERNATE_TIME_MS) until connection is established
-const bool IDLE_MODE = true; //  goes in power save mode, with periodic polling (IDLEMODE_TIME_MS) until OSC message switches to a different permormance mode.
-
+const bool SAVE_POWER_AFTER_CONNECTION_TIMEOUT = true;
 const int CONNECTION_TIMEOUT_MS = 120000;
-const int IDLEMODE_TIME_MS = 30000;
+const bool IDLE_MODE = true;
+const int IDLEMODE_CHECK_INTERVAL_MS = 30000;
 const int HYBERNATE_TIME_MS = 60000;
-unsigned long REFRESH_MS  = 100;
-unsigned long REFRESH_BATTERY_MS = 120000;
-
 const bool SERIAL_PRINT = false; 
 const bool BATTERY_PRINT = false;
-
+unsigned long REFRESH_MS  = 100;
+unsigned long REFRESH_BATTERY_MS = 120000;
 const uint8_t LED_BRIGHTNESS = 255/3;
 const int IDLE_CPU_FREQUENCY = 80;
 const int LO_POWER_CPU_FREQUENCY = 160;
 const int NORMAL_CPU_FREQUENCY = 240;
 
 // Array of SSIDs and PASSWORDS 
-const char* SSIDS[] = {"LeftBehinds", "EXT2-CommunityFibre10Gb_36CD6", "BT-FCACXW"};
-const char* PASSWORDS[] = {"DontLeaveMeBehind", "ccucykwier", "rb7bQYfghCdPMx"};
+const char* SSIDS[] = {"LeftBehinds", "OtherNetwork_SSID"};
+const char* PASSWORDS[] = {"DontLeaveMeBehind", "OtherNetwork_PW"};
 
 // Enum for network indices
 enum NetworkIndex {
   LEFTBEHINDS = 0,
-  OTHERNETWORK_A = 1,
-  OTHERNETWORK_B = 2
+  OTHERNETWORK= 1
 };
 const NetworkIndex selectedNetwork = LEFTBEHINDS;
 
@@ -77,7 +73,7 @@ const int BUTTON_0 = 8; //6
 const int BUTTON_1 = 3; //7
 const int BUTTON_2 = 4; //
 
-int ins[] = {FLEXO_0, FLEXO_1, BUTTON_0, BUTTON_1, BUTTON_2};
+int ins[] = {FLEXO_0, FLEXO_1, BUTTON_0, BUTTON_1, BUTTON_2}; // this is the routing for TINYS3_BUILT_1
 int insVal[] = {0, 0, 0, 0, 0};
 int insVal_prev[] = {0, 0, 0, 0, 0};
 
@@ -129,7 +125,7 @@ void setup() {
         connectionTimeOut_clocker = connectionTimeOut_clocker + wiFiLoop_ms; //hybernate
         if (connectionTimeOut_clocker >= CONNECTION_TIMEOUT_MS) {
             
-            if (HYBERNATE_MODE) {
+            if (SAVE_POWER_AFTER_CONNECTION_TIMEOUT) {
               Serial.println("Connection timeout. Hybernating...");
               hybernate();
               Serial.println("Waking up from hybernation...");
@@ -174,7 +170,7 @@ void loop() {
 void idleMode() {
     Serial.println("Entering power save (idle) with periodic polling...");
     setIdleMode();
-    delay((uint32_t)IDLEMODE_TIME_MS);
+    delay((uint32_t)IDLEMODE_CHECK_INTERVAL_MS);
 
     while (true) {
         
@@ -196,7 +192,7 @@ void idleMode() {
         }
         //setIdleMode();
         Serial.println("No wake up message received. Keep staying in power save mode");
-        delay(IDLEMODE_TIME_MS);       
+        delay(IDLEMODE_CHECK_INTERVAL_MS);       
     }
 }
 
